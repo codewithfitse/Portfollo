d6649a7f9f1c698504659df2f3e4dd004043801d
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-24 21:26:38 +0300

    Changes

--- React-app/src/page/Home.jsx
+++ React-app/src/page/Home.jsx
@@ -50,7 +50,7 @@ const Home = () => {
         </div>
         <div className="w-auto h-auto items-center justify-items-center">
           <img
-            src="/portfolioHeroBg.jpg"
+            src="/portfolioHeroBg.png"
             className="w-full h-full"
             alt="Profile"
           />
