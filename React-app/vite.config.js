6f12f6c2e10c76a0c46e3cdc1a416424bb61c791
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-01 21:27:53 +0300

    add project called movie fetching

--- React-app/vite.config.js
+++ React-app/vite.config.js
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
+  build: {
+    rollupOptions: {
+      external: ["CtrlF"], // <-- this line tells Vite to ignore bundling CtrlF
+    },
+  },
 });
