2b475bc26871d961dab4296da6b1b3456f5680df
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-01 22:00:49 +0300

    add css

--- React-app/src/page/Project.jsx
+++ React-app/src/page/Project.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const Project = () => {
   return (
     <>
-      <div className="w-full h-[100vh] flex flex-col items-center pt-[50px] px-2">
+      <div className="w-full h-[100vh] flex flex-col items-center pt-[50px] px-2 background">
         <h1 className="text-5xl font-bold">Project</h1>
         <div className="w-full grid grid-cols-2 gap-2">
           <div className="w-full h-fit flex flex-col items-center mt-10 p-5 bg-[#323946] border-2 border-[#1f242d] hover:border-[#0ef] hover:transform-scale(1.02) rounded-2xl">
