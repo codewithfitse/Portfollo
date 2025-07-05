cbe3003978399a21897f2ed2e0ddafca7882304f
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-05 22:47:11 +0300

    UPDATED THE SEVICE

--- React-app/src/page/Service.jsx
+++ React-app/src/page/Service.jsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 export const Service = () => {
   return (
-    <section className="w-full h-full background">
+    <section className="w-full h-full overflow-hidden background">
       <Header />
       <div className="flex flex-col w-full items-center px-1">
-        <h1 className="my-[50px] font-bold text-[56px]">
+        <h1 className="my-[50px] font-bold text-[30px] lg:text-[56px]">
           Our <span className="text-[#0ef]">Service</span>
         </h1>
-        <div className="w-full grid lg:grid-cols-2  space-y-3 space-x-2">
-          <div className="w-auto flex flex-1 flex-col items-center border-2 border-[#1f242d] rounded-2xl p-10 bg-[#323946] hover:border-[#0ef] hover:transform-scale(1.02)">
-            <h2 className="text-[40px] font-bold mb-2">Web Development</h2>
-            <p className="w-[400px] mb-6">
+        <div className="w-full grid lg:grid-cols-2 space-y-3 space-x-2">
+          <div className="w-fit lg:w-full flex flex-1 flex-col items-center border-2 border-[#1f242d] rounded-2xl p-10 bg-[#323946] hover:border-[#0ef] hover:transform-scale(1.02)">
+            <h2 className="text-[20px] lg:text-[40px] font-bold mb-2">
+              Web Development
+            </h2>
+            <p className="w-[80%] mb-6">
               I am professionally educated developer in the world. i have worked
               for many years in artificial intelligence & Ethiopian cyberspace.
             </p>
@@ -22,7 +24,7 @@ export const Service = () => {
           </div>
           <div className="w-full flex flex-col  items-center border-2 border-[#1f242d] rounded-2xl p-6 bg-[#323946] hover:border-[#0ef] hover:transform-scale(1.02)">
             <h2 className="text-[40px] font-bold mb-2">Graphic design</h2>
-            <p className="w-[400px] mb-6">
+            <p className="w-[80] mb-6">
               I am professionally educated developer in the world. i have worked
               for many years in artificial intelligence & Ethiopian cyberspace.
             </p>
@@ -32,7 +34,7 @@ export const Service = () => {
           </div>
           <div className="w-full flex flex-1 flex-col items-center border-2 border-[#1f242d] rounded-2xl p-6 bg-[#323946] hover:border-[#0ef] hover:transform-scale(1.02)">
             <h2 className="text-[40px] font-bold mb-2">Digital Marketing</h2>
-            <p className="w-[400px] mb-6">
+            <p className="w-[80] mb-6">
               I am professionally educated developer in the world. i have worked
               for many years in artificial intelligence & Ethiopian cyberspace.
             </p>
