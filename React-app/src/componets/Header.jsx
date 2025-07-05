9d6cdb7613102ea423aef806074921545d162f47
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-05 22:09:55 +0300

    UPDATED THE HEADER

--- React-app/src/componets/Header.jsx
+++ React-app/src/componets/Header.jsx
@@ -1,26 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className='px-[30px] py-[20px] flex justify-between items-center'>
-        <div><h1 className='text-[25px] font-semibold text-white'>Portfollo.</h1></div>
-        <nav className='flex gap-8'>
-          <Link to={'/'}><h1 className='text-[#0ef] uppercase font-medium text-xl transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]'>Home</h1></Link>
-          <Link to={'/About'}><h1 className='text-[#0ef] uppercase font-medium text-xl transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]'>About</h1></Link>
-          <Link to={'/Service'}><h1 className='text-[#0ef] uppercase font-medium text-xl transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]'>Service</h1></Link>
-          <Link to={'/Contact'}><h1 className='text-[#0ef] uppercase font-medium text-xl transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]'>Contact</h1></Link>
-          <Link to={'/Login'}><h1 className='text-[#0ef] uppercase font-medium text-xl transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]'>Login</h1></Link>
-        </nav>
-      </header>
-  )
-}
-
-
-
-
-
-
-
-
-
+    <header className="w-full h-[70px] py-[20px] flex justify-between items-center">
+      <div className="w-full">
+        <h1 className="text-[13px] lg:text-[25px] font-semibold text-white">
+          Portfollo.
+        </h1>
+      </div>
+      <nav className="flex gap-2.5 lg:gap-8 text-[10px] lg:text-[20px]">
+        <Link to={"/"}>
+          <h1 className="text-[#0ef] text-[10px] lg:text-[20px] uppercase font-medium transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]">
+            Home
+          </h1>
+        </Link>
+        <Link to={"/About"}>
+          <h1 className="text-[#0ef] text-[10px] lg:text-[20px] uppercase font-medium transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]">
+            About
+          </h1>
+        </Link>
+        <Link to={"/Service"}>
+          <h1 className="text-[#0ef] text-[10px] lg:text-[20px] uppercase font-medium transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]">
+            Service
+          </h1>
+        </Link>
+        <Link to={"/Contact"}>
+          <h1 className="text-[#0ef] text-[10px] lg:text-[20px] uppercase font-medium transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]">
+            Contact
+          </h1>
+        </Link>
+        <Link to={"/Login"}>
+          <h1 className="text-[#0ef] text-[10px] lg:text-[20px] uppercase font-medium transition-all duration-500 hover:scale-150 hover:filter-none hover:opacity-100 animate-[slidetop_0.5s_ease_forwards]">
+            Login
+          </h1>
+        </Link>
+      </nav>
+    </header>
+  );
+};
